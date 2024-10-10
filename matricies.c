14b449b2feae7a60e1ae5777e8c3cdf1dce48708
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-10-10 17:08:32 -0500

    Lab 5.2

--- matricies.c
+++ matricies.c
@@ -34,22 +34,24 @@ int main()
 {
     int m1[SIZE][SIZE]; 
     int m2[SIZE][SIZE]; 
-    for (int i=0; i<SIZE; i++)
+    for (int i=0; i<SIZE; i++) //goes through each row of matrix 1... 
     {
-        int temp[SIZE]; 
-        printf("give row %d of matrix, with each integer seperated by spaces: ", i+1); 
-        for (int j=0; j<SIZE; j++) //will read each int seperatley becuase white space seperates them 
+        printf("give row %d of matrix 1, with each integer seperated by spaces: ", i+1); 
+        for (int j=0; j<SIZE; j++) //...and for every column...
         {
-            scanf("%d", &temp[j]); 
+            scanf("%d", &m1[i][j]); //...read each int seperatley becuase white space seperates them 
         }
+
     }
     
-    
-
-//     printf("array: ");
-//     for (int i=0; i<SIZE; i++) 
-//     {
-//         printf("%d ", temp[i]);
-//     }
-//     return 0;    
+    printf("The resulting 2D matrix is:\n"); //prints 2D array in pretty format 
+    for (int i=0; i<SIZE; i++) 
+    {
+        printf("|");
+        for (int j=0; j<SIZE; j++) 
+        {
+            printf("%d ", m1[i][j]);
+        }
+        printf("|\n");
+    }  
 }
