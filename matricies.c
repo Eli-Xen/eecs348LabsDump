154070e7a3c28673810d27a3dcbfab40259a8d2a
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-10-10 16:00:02 -0500

    Lab 5.2

---
+++ matricies.c
@@ -0,0 +1,37 @@
+// Author: Eliza Malyshev 
+// KUID: 3122318
+// Date: 10/10/2024 
+// Lab: #5.2
+// Purpose: make and manipulate 2D arrays 
+#include <stdio.h> 
+#define SIZE 5 
+
+int addMx(int m1[], int m2[])
+{
+    
+    return 0;    
+}
+
+int multMx(int m1[], int m2[])
+{
+
+    return 0;    
+}
+
+int transMx(int m1[], int m2[])
+{
+
+    return 0;    
+}
+
+int printMx(int m1[], int m2[])
+{
+
+    return 0;    
+}
+
+int main()
+{
+
+    return 0;    
+}
