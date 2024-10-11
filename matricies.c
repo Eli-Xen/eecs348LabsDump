891e3a2a59a70e0a87ae65fa01ee423ade33b577
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-10-10 22:52:26 -0500

    Lab 5.2

--- matricies.c
+++ matricies.c
@@ -79,8 +79,21 @@ int main()
         }
 
     }
+    printf("\n"); 
+    for (int i=0; i<SIZE; i++) //goes through each row of matrix 2... 
+    {
+        printf("give row %d of matrix 2, with each integer seperated by spaces: ", i+1); 
+        for (int j=0; j<SIZE; j++) //...and for every column...
+        {
+            scanf("%d", &m2[i][j]); //...read each int seperatley becuase white space seperates them 
+        }
 
-    int (*added)[SIZE]=addMx(m1, m2); //uses function pointer to call function and take return of pointer 
-    printMx(added); //pass pointer of matrix to be printed 
+    }
 
+    // int (*added)[SIZE]=addMx(m1, m2); //uses function pointer to call function and take return of pointer 
+    // printf("matricies added together look like: \n"); 
+    // printMx(added); //pass pointer of matrix to be printed 
+    printf("matricies multiplied together look like: \n"); 
+    int (*multiplied)[SIZE]=multMx(m1, m2); 
+    printMx(multiplied); 
 }
