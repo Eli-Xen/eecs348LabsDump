7a975ba155e8960b70f6642662114037144ae60a
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-10-26 19:30:36 -0500

    Lab 7

---
+++ functions.js
@@ -0,0 +1,39 @@
+
+
+function change_color() {
+  var paraR = document.getElementById("paragraph_R").value;
+  var paraG = document.getElementById("paragraph_G").value;
+  var paraB = document.getElementById("paragraph_B").value;
+  var borderWidth = document.getElementById("border_width").value;
+  var backR = document.getElementById("backR").value;
+  var backG = document.getElementById("backG").value;
+  var backB = document.getElementById("backB").value;
+
+  var tag = document.getElementById("paragraph");
+  tag.style.borderColor = `rgb(${paraR},${paraG},${paraB})`;
+  //document.getElementById("paragraph").style.borderWidth = "200px";
+  tag.style.borderWidth = `${borderWidth}px`; 
+  tag.style.backgroundColor = `rgb(${backR},${backG},${backB})`;
+  
+}
+
+function checkPass()
+{
+  var p1 = document.getElementById("pass1").value;
+  var p2 = document.getElementById("pass2").value;
+  if ((p1.length<8) || (p2.length<8))
+  {
+    alert("passwords have to be at least 8 characters")
+  }
+  else if (p1!=p2)
+  {
+    alert("passwords dont match")
+  }
+  else if (p1==p2)
+  {
+    alert("passwords match and at least 8 characters")
+  }
+  else
+  alert("hmmmm smth went wrong")
+    //x = document.getElementById("myForm").length;
+}
---
+++ practice4.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+here is your table: 
+
+<?php
+$size=(int)$_POST['size']; //takes in value for size from html that calls it
+echo "<table border='1'>";
+
+echo "<tr><td></td>"; //corner header cell 
+for ($c=1; $c<=$size; $c++) //column headers 
+{
+    echo "<td><strong>$c</strong></td>"; 
+}
+echo "</tr>";
+
+for ($i=1; $i<=$size; $i++) {
+    echo "<tr>";
+    echo "<td><strong>$i</strong></td>"; //row headers 
+    for ($j=1; $j<=$size; $j++) {
+	$x=$i*$j; 
+        echo "<td>$x</td>";
+    }
+    echo "</tr>";
+}
+
+echo "</table>";
+?>
+
+</body>
+</html>
